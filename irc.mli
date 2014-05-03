6b95aeb2b8c571b75f1a10f72c5f7375d2ad2ca0
Aaron France <aaron.l.france@gmail.com>
2014-05-03 13:23:23 +0200

    Add better irc.mli.

--- irc.mli
+++ irc.mli
@@ -2,9 +2,18 @@ open Core.Std
 
 
 module Message : sig
-
-  type t
-
-  val message_of_string : string -> t
-
-end
+  type msg = { channel : string;
+               nick : string;
+               message : string;
+               date : float;
+             }
+  type info = { channel : string;
+                message : string;
+                date : float;
+              }
+  type t = Msg of msg | Info of info | Invalid of string
+  val of_string : raw:Core.Std.String.t -> channel:string -> t
+  val of_file : string -> (t Core.Std.List.t, exn) Core.Std.Result.t
+  val parse_directory :
+    string -> (t Core.Std.List.t, exn) Core.Std.Result.t Core.Std.Array.t
+end      
