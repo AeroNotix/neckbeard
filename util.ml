9b6e45ce430f61eaf2d608ef78e1c1f7bfe75fd7
Aaron France <aaron.l.france@gmail.com>
2014-05-03 15:42:35 +0200

    Cons the base directory onto the list of directories.

--- util.ml
+++ util.ml
@@ -10,13 +10,15 @@ let rec readdir ?(recurse=false) base =
   let rec aux acc = function
     | [] -> List.rev acc
     | hd :: tl when is_directory hd ->
-       aux (hd :: (readdir ~recurse:recurse hd @ acc)) tl
+       aux (readdir ~recurse:recurse hd @ acc) tl
     | hd :: tl -> aux (hd :: acc) tl in
-  let topbase = Array.map
-               ~f:(fun file -> Filename.concat base file)
-               (Sys.readdir base)
-             |> Array.to_list in
-  if recurse then
-    topbase @ aux [] topbase
-  else
-    topbase
+  let topdir =
+    Array.map
+      ~f:(fun file -> Filename.concat base file)
+      (Sys.readdir base)
+    |> Array.to_list in
+  base ::
+    if recurse then
+      topdir @ aux [] topdir
+    else
+      topdir
